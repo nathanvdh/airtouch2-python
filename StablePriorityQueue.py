49491d3650490c0c875f157b72b2b2b32912c410
Nathan Van der Hoek <nvdh@pop-os.localdomain>
2022-04-10 15:59:43 +0930

    comments

--- StablePriorityQueue.py
+++ StablePriorityQueue.py
@@ -1,7 +1,7 @@
 from queue import PriorityQueue
 from sys import maxsize as MAX_INT
 
-# Need to make PriorityQueue stable, see: https://docs.python.org/3/library/heapq.html#priority-queue-implementation-notes
+#  To make PriorityQueue stable, see: https://docs.python.org/3/library/heapq.html#priority-queue-implementation-notes
 # The item in the queue is: (priority, count, Message)
 class StablePriorityQueue(PriorityQueue):
     def __init__(self):
@@ -10,7 +10,7 @@ class StablePriorityQueue(PriorityQueue):
 
     def put(self, item, priority: int, block=True, timeout=None):
         super().put((priority, self.count, item), block=block, timeout=timeout)
-        self.count = (self.count + 1) % MAX_INT # force overflow to save memory, not sure if necessary
+        self.count = (self.count + 1) % MAX_INT # force overflow to prevent count growing infinitely, not sure if necessary
 
     def get(self, block=True, timeout=None):
         item = super().get(block=block, timeout=timeout)
