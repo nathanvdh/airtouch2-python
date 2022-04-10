0211c86b7c8cfebe8bb4f12a781936253b071634
Nathan Van der Hoek <nvdh@pop-os.localdomain>
2022-04-10 16:48:54 +0930

    comments and neatness

--- protocol/constants.py
+++ protocol/constants.py
@@ -1,5 +1,5 @@
 
-from enum import Enum, IntEnum
+from enum import IntEnum
 
 class MessageLength(IntEnum):
     UNDETERMINED = 0
@@ -7,18 +7,18 @@ class MessageLength(IntEnum):
     RESPONSE = 395
 
 # remove stuff from here if it's only ever used once
-# maybe also split it into a few sensical enums rather
-# than encompassing everything here
+# also split it into a few different, sensically grouped enums rather
+# than encompassing everything here (I've done this now with CommandMessageType)
 class CommandMessageConstants(IntEnum):
     BYTE_0 = 85                  # Byte 0 of command always fixed
     BYTE_2 = 12                  # Byte 2 of command always fixed
-    TOGGLE = 128          # Toggle on/off in byte 4
+    TOGGLE = 128                 # Toggle on/off in byte 4
     BYTE_4_GRP_POSDEC = 1        # Position decrement (5% down) in byte 4
     BYTE_4_GRP_POSINC = 2        # Position increment (5% up) in byte 4
     BYTE_4_AC_MODE = 129         # AC mode in byte 4
     BYTE_4_AC_FAN_SPD = 130      # AC Fan speed in byte 4
-    AC_TEMP_DEC = 147     # AC Temperature setpoint decrement in byte 4
-    AC_TEMP_INC = 163     # AC Temperature setpoint increment in byte 4
+    AC_TEMP_DEC = 147            # AC Temperature setpoint decrement in byte 4
+    AC_TEMP_INC = 163            # AC Temperature setpoint increment in byte 4
     BYTE_5_GRP_POS = 1           # In Position dec/inc command, byte 5 is fixed
 
 #e.g.
@@ -31,20 +31,20 @@ class ResponseMessageConstants(IntEnum):
     LONG_STRING_LENGTH = 16
     SHORT_STRING_LENGTH = 8
 
-# Thanks home-assistant.io user Radebe2k
+# Thanks to home-assistant.io user Radebe2k
 class ResponseMessageOffsets(IntEnum):
     # Header is 4 bytes
     HEADER = 0
 
     # There are 16 zones
-    # zone names are 8 bytes
+    # zone names are 8 bytes each
     ZONE_NAMES_START = 100
-    # zone statuses are 1 byte
+    # zone statuses are 1 byte each
     ZONE_STATUSES_START = 228
-    # Zone strengths are 1 byte
+    # Zone strengths are 1 byte each
     ZONE_STRENGTHS_START = 276
     
-    # 16 chars for system name
+    # System name is 16 bytes (ResponseMessageConstants.LONG_STRING_LENGTH)
     SYSTEM_NAME = 324
 
     # perhaps should just store the first offset and iterate from 1st to 2nd
