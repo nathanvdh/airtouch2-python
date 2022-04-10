ff1898c3269bdbf2a89965d3abbdb9cea12b6ebc
Nathan Van der Hoek <nvdh@pop-os.localdomain>
2022-04-10 14:00:47 +0930

    move priority queue stuff into a new class: StablePriorityQueue

---
+++ StablePriorityQueue.py
@@ -0,0 +1,15 @@
+from queue import PriorityQueue
+from sys import maxsize as MAX_INT
+
+class StablePriorityQueue(PriorityQueue):
+    def __init__(self):
+        super().__init__()
+        self.count = 0
+
+    def put(self, item, priority: int, block=True, timeout=None):
+        super().put((priority, self.count, item), block=block, timeout=timeout)
+        self.count = (self.count + 1) % MAX_INT # force overflow to save memory, not sure if necessary
+
+    def get(self, block=True, timeout=None):
+        item = super().get(block=block, timeout=timeout)
+        return item[2] # return the actual item that was put()
