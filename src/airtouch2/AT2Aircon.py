16fc10e47a8ba2c835e34079c9aa8beda112646c
nathanvdh <a1743599@student.adelaide.edu.au>
2022-11-25 20:40:12 +1030

    Fix fan speed values for units without Auto

--- src/airtouch2/AT2Aircon.py
+++ src/airtouch2/AT2Aircon.py
@@ -38,16 +38,17 @@ class AT2Aircon:
     #   Gateway ID 0x14 has no Auto
     #   Gateway IDs 0xFF with 3 speed have no Auto
     # These are based on the app decompiled code
-    def _set_true_and_supported_fan_speed(self, fan_speed: int, num_supported_speeds: int, gateway_id: int):
+    def _set_true_and_supported_fan_speed(self, speed_val: int, num_supported_speeds: int, gateway_id: int):
         all_speeds: list[ACFanSpeedReference] = list(ACFanSpeedReference._member_map_.values())
         # Unsure if this is sufficent or if I need to check the second/other type of brand
         if self.brand == ACBrand.FUJITSU and num_supported_speeds == 4:
-            self.supported_fan_speeds = all_speeds[:num_supported_speeds+1]
+            self.supported_fan_speeds = all_speeds[:5]
         else:
-            if (self.brand != ACBrand.DAIKIN and not (gateway_id == 0xFF and num_supported_speeds == 3) and gateway_id != 0x14):
-                self.supported_fan_speeds = [ACFanSpeedReference.AUTO]
-            else:
+            # Check special cases that don't support Auto mode
+            if (self.brand == ACBrand.DAIKIN or (gateway_id == 0xFF and num_supported_speeds == 3) or gateway_id == 0x14):
                 self.supported_fan_speeds = []
+            else:
+                self.supported_fan_speeds = [ACFanSpeedReference.AUTO]
 
             if num_supported_speeds > 2:
                 self.supported_fan_speeds += all_speeds[2:2+num_supported_speeds]
@@ -56,8 +57,11 @@ class AT2Aircon:
             elif num_supported_speeds < 2:
                 _LOGGER.warning(f"AC{self.number} reports less than 2 supported fan speeds, this is unusual - " + OPEN_ISSUE_TEXT)
 
-        if fan_speed < 5:
-            self.fan_speed = self.supported_fan_speeds[fan_speed]
+        if speed_val < 5:
+            # Units with no Auto speed still start with low = 1
+            if ACFanSpeedReference.AUTO not in self.supported_fan_speeds:
+                speed_val -= 1
+            self.fan_speed = self.supported_fan_speeds[speed_val]
         else:
             self.fan_speed = ACFanSpeedReference.AUTO
 
@@ -114,6 +118,9 @@ class AT2Aircon:
     def set_fan_speed(self, fan_speed: ACFanSpeedReference):
         if fan_speed in self.supported_fan_speeds:
             speed_val = self.supported_fan_speeds.index(fan_speed)
+            # Units with no Auto speed still start with low = 1
+            if ACFanSpeedReference.AUTO not in self.supported_fan_speeds:
+                speed_val += 1
             self._client.send_command(SetFanSpeed(self.number, speed_val))
         else:
             _LOGGER.warning(f"Cannot set fan speed to unsupported value {fan_speed}")
