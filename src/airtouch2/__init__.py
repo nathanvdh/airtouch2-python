a618db2dfb301f2a9b7dbcbaabfcad0a57e76e6a
nathanvdh <a1743599@student.adelaide.edu.au>
2022-12-01 21:58:21 +1030

    Add AT2Group to package import

--- src/airtouch2/__init__.py
+++ src/airtouch2/__init__.py
@@ -1,3 +1,4 @@
 from airtouch2.AT2Client import AT2Client
 from airtouch2.AT2Aircon import AT2Aircon
+from airtouch2.AT2Group import AT2Group
 from airtouch2.protocol.enums import ACFanSpeedReference, ACMode
