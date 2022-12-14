b7aa552d4ba40dd1437f6d979beaf89a12ca2447
nathanvdh <a1743599@student.adelaide.edu.au>
2022-12-14 12:23:41 +1030

    Properly fix type checking of command.serialize()

--- src/airtouch2/protocol/messages/CommandMessage.py
+++ src/airtouch2/protocol/messages/CommandMessage.py
@@ -1,12 +1,16 @@
 from airtouch2.protocol.messages.Message import Message
 from airtouch2.protocol.constants import CommandMessageConstants, MessageLength
-
+from abc import abstractmethod
 class CommandMessage(Message):
     """ Command message base class from which all airtouch2 command messages are derived"""
     length: MessageLength = MessageLength.COMMAND
 
     # command messages are only sent so only need to be serialized
-    def serialize(self, prefilled_msg: bytearray) -> bytearray:
+    def _serialize(self, prefilled_msg: bytearray) -> bytearray:
         prefilled_msg[0] = CommandMessageConstants.BYTE_0
         prefilled_msg[2] = CommandMessageConstants.BYTE_2
         return self.add_checksum(prefilled_msg)
+
+    @abstractmethod
+    def serialize(self) -> bytearray:
+        pass
--- src/airtouch2/protocol/messages/ac_commands.py
+++ src/airtouch2/protocol/messages/ac_commands.py
@@ -8,10 +8,11 @@ class ACCommand(CommandMessage):
         super().__init__()
         self.target_ac = target_ac_number
 
-    def serialize(self, prefilled_msg: bytearray) -> bytearray:
+    def _serialize(self, prefilled_msg: bytearray) -> bytearray:
         prefilled_msg[1] = CommandMessageType.AC_CONTROL
         prefilled_msg[3] = self.target_ac
-        return super().serialize(prefilled_msg)
+        return super()._serialize(prefilled_msg)
+
 
 class ChangeSetTemperature(ACCommand):
     """Command to increment or decrement the AC set point by 1 degree"""
@@ -22,7 +23,7 @@ class ChangeSetTemperature(ACCommand):
     def serialize(self) -> bytearray:
         serial_msg: bytearray = bytearray(self.length)
         serial_msg[4] = ACCommands.TEMP_INC if self.inc else ACCommands.TEMP_DEC
-        return super().serialize(serial_msg)
+        return super()._serialize(serial_msg)
 
 class ToggleAC(ACCommand):
     """Command to toggle an AC on or off"""
@@ -30,7 +31,7 @@ class ToggleAC(ACCommand):
     def serialize(self) -> bytearray:
         serial_msg: bytearray = bytearray(self.length)
         serial_msg[4] = CommandMessageConstants.TOGGLE
-        return super().serialize(serial_msg)
+        return super()._serialize(serial_msg)
 
 # needs investigation
 class SetFanSpeed(ACCommand):
@@ -43,7 +44,7 @@ class SetFanSpeed(ACCommand):
         serial_msg: bytearray = bytearray(self.length)
         serial_msg[4] = ACCommands.SET_FAN_SPEED
         serial_msg[5] = self.fan_speed
-        return super().serialize(serial_msg)
+        return super()._serialize(serial_msg)
 
 class SetMode(ACCommand):
     """Command to set the AC mode to one of those in ACMode"""
@@ -55,4 +56,4 @@ class SetMode(ACCommand):
         serial_msg: bytearray = bytearray(self.length)
         serial_msg[4] = ACCommands.SET_MODE
         serial_msg[5] = self.mode
-        return super().serialize(serial_msg)
+        return super()._serialize(serial_msg)
