b5b0b1be6a2df4f9c23d3a9bfc8da2b843499610
nathanvdh <a1743599@student.adelaide.edu.au>
2023-07-10 21:08:15 +0930

    Separate at2 and at2plus imports

---
+++ src/airtouch2/at2/__init__.py
@@ -0,0 +1,4 @@
+from airtouch2.at2.At2Client import At2Client
+from airtouch2.at2.At2Aircon import At2Aircon
+from airtouch2.at2.At2Group import At2Group
+from airtouch2.protocol.at2.enums import ACFanSpeedReference, ACMode
---
+++ src/airtouch2/at2plus/__init__.py
@@ -0,0 +1,3 @@
+from airtouch2.at2plus.At2PlusClient import At2PlusClient
+from airtouch2.at2plus.At2PlusAircon import At2PlusAircon
+from airtouch2.protocol.at2plus.enums import AcSetMode, AcMode, AcFanSpeed
